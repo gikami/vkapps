26911c2b62a6f8675c154253965165f0613410fa
Альберт Ушаков <gikami.book@gmail.com>
2020-06-29 19:00:25 +0300

    Updates

---
+++ precache-manifest.82706e08380bbd381c28d3f17450714f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21525cd10ebee9200594fe3c67f61af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f55b091ccaca456f6123",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "fd044334dc64fb25fff8",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f55b091ccaca456f6123",
+    "url": "./static/js/2.5a791fc9.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.5a791fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd044334dc64fb25fff8",
+    "url": "./static/js/main.83ece7c5.chunk.js"
+  },
+  {
+    "revision": "4986fdbbb0264c9f8bb0",
+    "url": "./static/js/runtime-main.b73a09a2.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./vkapps/precache-manifest.cbc4e566dd8fd92661b9eec6ac588fcd.js"
+  "./precache-manifest.82706e08380bbd381c28d3f17450714f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./vkapps/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
