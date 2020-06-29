572aee61f6c1f81be8830bb7edf847d002bab107
Альберт Ушаков <gikami.book@gmail.com>
2020-06-29 18:50:53 +0300

    Updates

---
+++ precache-manifest.0d08fdde726feb6418636d3defdcc67f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce62b02a22fcd7eecc65e978efeca82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d89d376cf70b522b7112",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "fd044334dc64fb25fff8",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "d89d376cf70b522b7112",
+    "url": "/static/js/2.8a49c1b2.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.8a49c1b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd044334dc64fb25fff8",
+    "url": "/static/js/main.83ece7c5.chunk.js"
+  },
+  {
+    "revision": "2ece53392cb332ec66d4",
+    "url": "/static/js/runtime-main.70d187c3.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
